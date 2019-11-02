7c847bcce9a2f9bcc996fab4712049fda849a381
Benedikt <mail@benedikt.gr>
2019-11-02 15:29:55 +0100

    add htaccess, cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const moment = require("moment");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/.htaccess");
 
   return {
     dir: { input: "src", output: "dist", data: "_data" },
---
+++ src/legal.md
@@ -0,0 +1,9 @@
+---
+layout: layouts/main.njk
+---
+
+# Legal Information
+
+Benedikt Gregor Nützel  
+Selkestr.1  
+12051 Berlin
