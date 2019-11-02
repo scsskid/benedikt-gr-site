1e5083c3e01e26dd6323f92cd13bbfdd4d1e73eb
Benedikt <mail@benedikt.gr>
2019-11-02 14:53:56 +0100

    small tpl changes

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,20 +1,20 @@
 module.exports = {
-	title: "Benedikt Gregors Website",
-	description: "",
-	url: "https://benedikt.gr",
-	baseUrl: "/",
-	author: "Name Surname",
-	authorTwitterUrl: "https://twitter.com/scsskid",
-	authorTwitterHandle: "@scsskid",
-	buildTime: new Date(),
-	buildTimeString: new Date().toLocaleDateString("en-US", {
-		year: "numeric",
-		month: "long",
-		day: "numeric"
-	}),
-	availability: {
-		status: true,
-		text: "available for projects",
-		date: "november 2019"
-	}
+  title: "Benedikt Gregors Website",
+  description: "",
+  url: "https://benedikt.gr",
+  baseUrl: "/",
+  author: "Name Surname",
+  authorTwitterUrl: "https://twitter.com/scsskid",
+  authorTwitterHandle: "@scsskid",
+  buildTime: new Date(),
+  buildTimeString: new Date().toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  }),
+  availability: {
+    status: true,
+    text: "available for projects",
+    date: "november 2019"
+  }
 };
