83d2a0751f4f2564aadc2e195a9eece16912762d
scsskid <runbgn@gmail.com>
2019-11-12 00:47:15 +0100

    update availability

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,20 +1,20 @@
 module.exports = {
-  title: "Benedikt Gregors Website",
-  description: "",
-  url: "https://benedikt.gr",
-  baseUrl: "/",
-  author: "Name Surname",
-  authorTwitterUrl: "https://twitter.com/scsskid",
-  authorTwitterHandle: "@scsskid",
+  title: 'Benedikt Gregors Website',
+  description: '',
+  url: 'https://benedikt.gr',
+  baseUrl: '/',
+  author: 'Name Surname',
+  authorTwitterUrl: 'https://twitter.com/scsskid',
+  authorTwitterHandle: '@scsskid',
   buildTime: new Date(),
-  buildTimeString: new Date().toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric"
+  buildTimeString: new Date().toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
   }),
   availability: {
     status: true,
-    text: "available for projects",
-    date: "November 2019"
+    text: 'available',
+    date: 'Jan 2020'
   }
-};
+}
