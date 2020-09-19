a08882f60c8624e318de42d001ae5c41eab49d64
Benedikt Gregor <runbgn@gmail.com>
2020-09-19 22:44:14 +0200

    formatting

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,12 @@
-module.exports = ctx => ({
+module.exports = (ctx) => ({
   map: ctx.options.map,
   plugins: {
     'postcss-import': {},
     'postcss-url': { url: 'copy', useHash: true },
     'postcss-mixins': {},
     'postcss-preset-env': {
-      stage: 1
+      stage: 1,
     },
-    cssnano: ctx.env === 'production' ? {} : false
-  }
-})
+    cssnano: ctx.env === 'production' ? {} : false,
+  },
+});
